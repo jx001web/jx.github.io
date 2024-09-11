4916971e21a0d1f5dceeab40f5642fb7beb683fa
jx001web <717257697@qq.com>
2024-09-11 20:04:42 +0800

    Add files via upload

---
+++ web/index.js
@@ -0,0 +1,12 @@
+// 设置图片切换
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if (mySrc === '1.jpg') {
+    myImage.setAttribute ('src','2.jpg');
+  } else {
+    myImage.setAttribute ('src','1.jpg');
+  }
+};
+
